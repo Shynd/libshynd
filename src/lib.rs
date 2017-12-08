241bf9e2e2ba9bb9fec325324b6a109a916170a2
Shynd <afexlabs@gmail.com>
2017-12-08 23:45:45 +0100

    Documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![crate_name = "libshynd"]
 #![crate_type = "lib"]
+#![doc(html_root_url = "https://shynd.xyz/libshynd/")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/Shynd/libshynd/master/logo.png")]
 
 /// This function returns true if the left-hand integer
 /// is divisible by the right-hand integer.
